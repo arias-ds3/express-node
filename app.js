106a4fa31185b19f3c127f84925ec43bf5cd9b41
arias-ds3 <ariasfa7@gmail.com>
2024-12-17 18:59:15 -0300

    response /

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 let app = express()
 
+app.get("/", (req, res)=>{
+    res.setDefaultEncoding("Hello")
+})
+
 app.listen(8081, ()=>{
     console.log("Server in 8081")
 })
