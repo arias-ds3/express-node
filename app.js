afeff36172ceb5096c0af3bbb07a255b4184522e
arias-ds3 <ariasfa7@gmail.com>
2024-12-17 21:39:57 -0300

    public directory

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 let app = express()
 
+app.use(express.static("public"))
+
+
 app.get("/", (req, res)=>{
-    res.setDefaultEncoding("Hello")
+    res.send("Hello")
 })
 
 app.listen(8081, ()=>{
